69dffbadf661938387a2aa57c1190e863a7b0719
venkat-nidrive <venkateswarlu@nidrive.jp>
2020-01-14 11:10:05 +0900

    Server and client implemented in single class with konduditservingmain call back.

--- java/src/main/java/ai/konduit/serving/examples/inference/InferenceModelStepKeras.java
+++ java/src/main/java/ai/konduit/serving/examples/inference/InferenceModelStepKeras.java
@@ -22,11 +22,18 @@ import ai.konduit.serving.config.Input;
 import ai.konduit.serving.config.Output;
 import ai.konduit.serving.config.ServingConfig;
 import ai.konduit.serving.configprovider.KonduitServingMain;
+import ai.konduit.serving.configprovider.KonduitServingMainArgs;
 import ai.konduit.serving.model.ModelConfig;
 import ai.konduit.serving.model.ModelConfigType;
 import ai.konduit.serving.pipeline.step.ModelStep;
+import ai.konduit.serving.verticles.inference.InferenceVerticle;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.commons.io.FileUtils;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.serde.binary.BinarySerde;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import java.io.File;
@@ -35,6 +42,8 @@ import java.nio.charset.Charset;
 /**
  * Example for Inference for KERAS ML model using Model step .
  * This illustrates only the server configuration and start server.
+ * Example for client test of Inference for Keras ML model using Model step .
+ * Unirest.post will call automatically on success of server start .
  */
 @NotThreadSafe
 public class InferenceModelStepKeras {
@@ -58,8 +67,10 @@ public class InferenceModelStepKeras {
                 .outputName("lstm_1")
                 .build();
 
+        int port = Util.randInt(1000, 65535);
+
         //ServingConfig set httpport and Input Formats
-        ServingConfig servingConfig = ServingConfig.builder().httpPort(3000).
+        ServingConfig servingConfig = ServingConfig.builder().httpPort(port).
                 inputDataFormat(Input.DataFormat.ND4J).
                 predictionType(Output.PredictionType.RAW).
                 build();
@@ -77,10 +88,40 @@ public class InferenceModelStepKeras {
         FileUtils.write(configFile, inferenceConfiguration.toJson(), Charset.defaultCharset());
 
         //Start inference server as per the above configurations
-        KonduitServingMain.main("--configPath", configFile.getAbsolutePath());
+        //KonduitServingMain.main("--configPath", configFile.getAbsolutePath());
+        KonduitServingMainArgs args1 = KonduitServingMainArgs.builder()
+                .configStoreType("file").ha(false)
+                .multiThreaded(false).configPort(port)
+                .verticleClassName(InferenceVerticle.class.getName())
+                .configPath(configFile.getAbsolutePath())
+                .build();
 
-        //Set sleep to wait till server started before getting any request from clients.
-        Thread.sleep(3600000);
+        //Set sleep to wait till server started before getting any request from clients if required.
+        //Thread.sleep(60000);
+
+        //Preparing input NDArray
+        INDArray arr = Nd4j.create(new float[]{1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f}, 1, 10);
+
+        //Create new file to write binary input data.
+        File file = new File("src/main/resources/data/test-input.zip");
+        System.out.println(file.getAbsolutePath());
+
+        BinarySerde.writeArrayToDisk(arr, file);
+
+        //Callback function  onSuccess Unirest client call.
+        KonduitServingMain.builder()
+                .onSuccess(() -> {
+                    try {
+                        String response = Unirest.post("http://localhost:" + port + "/raw/nd4j")
+                                .field("input", file)
+                                .asString().getBody();
+                        System.out.print(response);
+                    } catch (UnirestException e) {
+                        e.printStackTrace();
+                    }
+                })
+                .build()
+                .runMain(args1.toArgs());
 
     }
 }
