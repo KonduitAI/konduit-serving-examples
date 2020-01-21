b887f819daba0a1f278b43e07416aa56698da92b
venkat-nidrive <venkateswarlu@nidrive.jp>
2020-01-21 13:46:47 +0900

    WIP so delete. Test file is removed as it is included in Server file with callback.

--- java/src/main/java/ai/konduit/serving/examples/inference/InferenceModelStepONNX.java
+++ java/src/main/java/ai/konduit/serving/examples/inference/InferenceModelStepONNX.java
@@ -17,19 +17,29 @@
 package ai.konduit.serving.examples.inference;
 
 import ai.konduit.serving.InferenceConfiguration;
-import ai.konduit.serving.config.Input;
-import ai.konduit.serving.config.Output;
 import ai.konduit.serving.config.ServingConfig;
 import ai.konduit.serving.configprovider.KonduitServingMain;
 import ai.konduit.serving.model.PythonConfig;
+import ai.konduit.serving.pipeline.step.ImageLoadingStep;
 import ai.konduit.serving.pipeline.step.PythonStep;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.commons.io.FileUtils;
+import org.datavec.api.writable.NDArrayWritable;
+import org.datavec.api.writable.Writable;
+import org.datavec.image.transform.ImageTransformProcess;
+import org.datavec.python.PythonVariables;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.io.ClassPathResource;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import java.io.File;
 import java.nio.charset.Charset;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.bytedeco.numpy.presets.numpy.cachePackages;
 
 @NotThreadSafe
 
@@ -43,36 +53,61 @@ class InferenceModelStepONNX {
         //File path for model
         String working_dir = new ClassPathResource(".").getFile().getAbsolutePath();
         String python_code = "";
+        String pythonCodePath = new ClassPathResource("scripts/onnxFacedetect.py").getFile().getAbsolutePath();
 
-        //Set the python inputs and outputs
-        HashMap<String, String> python_inputs = new HashMap<>();
-        HashMap<String, String> python_outputs = new HashMap<>();
-        python_inputs.put("image", "NDARRAY");
-        python_outputs.put("boxes", "NDARRAY");
+
+        String pythonPath = Arrays.stream(cachePackages())
+                .filter(Objects::nonNull)
+                .map(File::getAbsolutePath)
+                .collect(Collectors.joining(File.pathSeparator));
 
         //python configuration for input and output.
         PythonConfig python_config = PythonConfig.builder()
-                .pythonCode(python_code)
-                .pythonInputs(python_inputs)
-                .pythonOutputs(python_outputs)
-                .pythonPath(working_dir)
+                .pythonCodePath(pythonCodePath)
+                .pythonInput("inputimage", PythonVariables.Type.NDARRAY.name())
+                .pythonOutput("boxes", PythonVariables.Type.NDARRAY.name())
+                .pythonPath("C:\\Users\\venkat-nidrive\\AppData\\Local\\Programs\\Python\\Python37\\python37.zip;" +
+                        "C:\\Users\\venkat-nidrive`AppData\\Local\\Programs\\Python\\Python37\\" +
+                        "DLLs;C:\\Users\\venkat-nidrive\\AppData\\Local\\Programs\\Python\\Python37\\" +
+                        "lib;C:\\Users\\venkat-nidrive\\AppData\\Local\\Programs\\Python\\Python37;" +
+                        "C:\\Users\\venkat-nidrive\\AppData\\Local\\Programs\\Python\\Python37\\lib\\" +
+                        "site-packages;C:\\Users\\venkat-nidrive\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages\\" +
+                        "pyyaml-5.2-py3.7-win-amd64.egg;c:\\projects\\konduit-new\\konduit-serving\\python")
+
+
                 .build();
 
-        String input_name = "input1";
         //Set the configuration of python to step
-        PythonStep onnx_step = PythonStep.builder()
-                .inputName(input_name)
-                .pythonConfig("python_config", python_config).build();
+        PythonStep onnx_step = new PythonStep().step(python_config);
 
         //ServingConfig set httpport and Input Formats
-        int port = Util.randInt(1000, 65535);
-        ServingConfig servingConfig = ServingConfig.builder().httpPort(3000).
-                //  outputDataFormat(Output.DataFormat.NUMPY).
-                build();
+        //int port = Util.randInt(1000, 65535);
+        int port = 3000;
+        ServingConfig servingConfig = ServingConfig.builder()
+                .httpPort(port)
+                .build();
+
+        ImageTransformProcess imageTransformProcess = new ImageTransformProcess.Builder()
+                .scaleImageTransform(20.0f)
+                //.resizeImageTransform(28,28)
+                .build();
+
+        ImageLoadingStep imageLoadingStep = ImageLoadingStep.builder()
+                .imageProcessingInitialLayout("NCHW")
+                .imageProcessingRequiredLayout("NHWC")
+                .inputName("inputimage")
+                .dimensionsConfig("inputimage", new Long[]{28L, 28L, 3L}) // Height, width, channels
+                .imageTransformProcess("inputimage", imageTransformProcess)
+                .build();
+       /* //Model config and set model type as KERAS
+        ImageLoadingStep imageLoadingStep = ImageLoadingStep.builder()
+                .inputName("inputimage")
+              //  .dimensionsConfig("default", new Long[]{240L, 320L, 2L}) // Height, width, channels
+                .build();*/
 
         //Inference Configuration
         InferenceConfiguration inferenceConfiguration = InferenceConfiguration.builder()
-                .step(onnx_step).servingConfig(servingConfig).build();
+                .steps(Arrays.asList(imageLoadingStep, onnx_step)).servingConfig(servingConfig).build();
 
         //Print the configuration to make sure our settings correctly set.
         System.out.println(inferenceConfiguration.toJson());
@@ -80,12 +115,32 @@ class InferenceModelStepONNX {
         File configFile = new File("config.json");
         FileUtils.write(configFile, inferenceConfiguration.toJson(), Charset.defaultCharset());
 
+        //Preparing input images.
+        // File imagePath = new File("src/main/resources/data/facedetector/1.jpg");
+        File imagePath = new ClassPathResource("data/facedetector/1.jpg").getFile();
+        Writable[][] output = imageLoadingStep.createRunner().transform(imagePath.toString());
+
+        INDArray rand_image = ((NDArrayWritable) output[0][0]).get();
+
         //Start inference server as per the above configurations
-        KonduitServingMain.main("--configPath", configFile.getAbsolutePath());
+        KonduitServingMain.builder()
+                .onSuccess(() -> {
+                    try {
+                        String response = Unirest.post("http://localhost:3000/raw/image")
+                                .field("inputimage", imagePath)
+                                .asString().getBody();
 
-        //Set sleep to wait till server started before getting any request from clients.
-        Thread.sleep(3600000);
+                        System.out.println(response);
 
+                        System.exit(0);
+                    } catch (UnirestException e) {
+                        e.printStackTrace();
+
+                        System.exit(0);
+                    }
+                })
+                .build()
+                .runMain("--configPath", configFile.getAbsolutePath());
     }
 
 }
