775a8ace024802093f7ad12f53d96647bdd2c8bf
shamsulazeem <shamsazeem20@gmail.com>
2021-03-16 16:32:47 +0800

    Adding example for creating and printing configurations

--- java/src/main/java/ai/konduit/serving/examples/basic/Example_1_CreatingConfig.java
+++ java/src/main/java/ai/konduit/serving/examples/basic/Example_1_CreatingConfig.java
@@ -1,11 +1,53 @@
 package ai.konduit.serving.examples.basic;
 
+import ai.konduit.serving.pipeline.api.pipeline.Pipeline;
+import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
+import ai.konduit.serving.pipeline.impl.step.logging.LoggingStep;
 import ai.konduit.serving.vertx.config.InferenceConfiguration;
 
+// Similar to what `konduit config --pipeline logging`
 public class Example_1_CreatingConfig {
     public static void main(String[] args) {
-        InferenceConfiguration inferenceConfiguration = new InferenceConfiguration();
+        // Printing a Json configuration for logging in a sequence pipeline
+        SequencePipeline sequencePipelineWithLoggingStep = SequencePipeline
+                .builder()
+                .add(new LoggingStep())
+                .build();
 
-        System.out.println(inferenceConfiguration.toJson());
+        System.out.format("----------%n" +
+                        "Pipeline with a Logging step output%n" +
+                        "------------%n" +
+                        "%s%n" +
+                        "------------%n%n",
+                sequencePipelineWithLoggingStep.toJson());
+
+        // Printing an empty or default InferenceConfiguration
+        InferenceConfiguration defaultInferenceConfiguration = new InferenceConfiguration();
+
+        System.out.format("----------%n" +
+                        "Default inference configuration%n" +
+                        "------------%n" +
+                        "%s%n" +
+                        "------------%n%n",
+                defaultInferenceConfiguration.toJson());
+
+        // Printing an InferenceConfiguration with a logging step sequence pipeline
+        InferenceConfiguration inferenceConfigurationWithPipeline = new InferenceConfiguration();
+        inferenceConfigurationWithPipeline.pipeline(sequencePipelineWithLoggingStep);
+
+        System.out.format("----------%n" +
+                        "Inference configuration with Logging step sequence pipeline%n" +
+                        "------------%n" +
+                        "%s%n" +
+                        "------------%n%n",
+                inferenceConfigurationWithPipeline.toJson());
+
+        // Printing InferenceConfiguration in YAML
+        System.out.format("----------%n" +
+                        "Inference Configuration in YAML%n" +
+                        "------------%n" +
+                        "%s%n" +
+                        "------------%n%n",
+                inferenceConfigurationWithPipeline.toYaml());
     }
 }
